75c943132eeb56b9450e98815d3db4498d00898f
Pawel Wujczyk <pwujczyk@gmail.com>
2024-10-25 21:10:05 +0200

    get webhooks -delete

--- WebHooks.ps1
+++ WebHooks.ps1
@@ -1,17 +1,66 @@
-function GetWebhooksUrls{
+function GetWebhooks {
     param(
         [string]$FullRepositoryName
     )
 
-    $fullRepositoryNameUrl="repos/${FullRepositoryName}/hooks"
-    $responseArray=$( gh api $fullRepositoryNameUrl ) | ConvertFrom-Json
-    $webhooksUrls=@()
-    foreach($response in $responseArray)
-    {
-        $url=$response.config.url;
-        $webhooksUrls+= $url;
+    $fullRepositoryNameUrl = "repos/${FullRepositoryName}/hooks"
+    $responseArray = $( gh api $fullRepositoryNameUrl ) | ConvertFrom-Json
+    $webhooks = @()
+    foreach ($response in $responseArray) {
+        $webook = [PSCustomObject]@{
+            url = $response.config.url
+            id  = $response.id
+        }
+
+        $webhooks += $webook;
     }
-    Write-Output $webhooksUrls
+   
+    return $webhooks
 }
 
-GetWebhooksUrls -FullRepositoryName "ProductivityTools-Transfers/ProductivityTools.Transfers.Api"
+function GetConfiguration {
+    param(
+        [string]$FileName
+    )
+    $config = Get-Content $FileName | ConvertFrom-Json
+    return $config
+}
+
+function DeleteWebHook {
+
+    param(
+        [string]$WebhookUrl
+    )
+    
+    gh api repos/ProductivityTools-Transfers/ProductivityTools.Transfers.Api/hooks --input - -X POST
+
+    Write-Output "Removing webhook $WebhookUrl"
+}
+function DeleteWebHooks {
+    param(
+        [string]$FullRepositoryName
+    )
+    $webhooksUrls = GetWebhooks -FullRepositoryName $FullRepositoryName
+    foreach ($webhook in $webhooks) {
+        DeleteWebHook -WebhookUrl $webhookUrl
+    }
+}
+
+function AddMissingWebhooks {
+
+}
+
+function Main {
+
+    $config = GetConfiguration -FileName "Configuration.json"
+    foreach ($repository in $config) {
+        DeleteWebHooks($repository.RepositoryName)
+        #Write-Output $repository.RepositoryName;
+        # Write-Output $repository.WebHooks
+    }
+
+
+}
+Main
+#$webhooksUrls=GetWebhooksUrls -FullRepositoryName "ProductivityTools-Transfers/ProductivityTools.Transfers.Api"
+#Write-Output $webhooksUrls
